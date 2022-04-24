ea13eb3f728c8717904548699a14307c8218a854
Brandon <brandon101morgan@outlook.com>
2022-04-23 22:32:37 -0500

    Sorting and reversing

---
+++ index.js
@@ -0,0 +1,18 @@
+//sort numbers from lowest to highest in this array
+var numbers = [3, 2, 5, 1, 8,];
+// uses the sort method to arrange the numbers low to high
+numbers.sort();
+//displays the answers
+alert(numbers);
+
+//create a string
+var str = "Brandon is lit";
+//declare a function to reverse said string
+function ReverseString(str)
+{
+    return str.split('').reverse().join('')
+}
+//display results in the console
+console.log(ReverseString(str));
+
+
